175359d1ee4f953ac15e19cade360d14ba450b86
NickJordan-BE <nikjordan525@gmail.com>
2025-11-26 15:31:33 -0800

    Update docker-compose with all services, update port mappings, and test container to container connectivity

--- services/orchestrator-service/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
+++ services/orchestrator-service/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
@@ -4,36 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 
 @RestController
 @SpringBootApplication
 public class RecsysOrchestratorServiceApplication {
- 	private WebClient webClient1;
- 	private WebClient webClient2;
-
-    public void MyService(WebClient.Builder webClientBuilder) {
-        this.webClient1 = webClientBuilder.baseUrl("http://localhost:8070").build();
-        this.webClient2 = webClientBuilder.baseUrl("http://localhost:8060").build();
-    }
-
-    public String callExternalApi() {
-        webClient1.get()
-                .uri("/")
-                .retrieve()
-                .bodyToMono(String.class);
-		webClient2.get()
-                .uri("/")
-                .retrieve()
-                .bodyToMono(String.class);
-		return "Done";
-	}
-
 
 	@RequestMapping("/")
 	public String home() {
-		callExternalApi();
 		return "DOne 2";
 	}
 
