bbeb84c885753671fd6c6ea2fd1ea3487ebcc20e
NickJordan-BE <nikjordan525@gmail.com>
2025-11-26 11:54:37 -0800

    Import and initialize Springboot services (orchestrator, ranking, fine-ranking)

---
+++ services/fine-ranking-service/src/main/java/com/example/recsys_fine_ranking_service/RecsysFineRankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.recsys_fine_ranking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecsysFineRankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecsysFineRankingServiceApplication.class, args);
+	}
+
+}
---
+++ services/fine-ranking-service/src/test/java/com/example/recsys_fine_ranking_service/RecsysFineRankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recsys_fine_ranking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecsysFineRankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
