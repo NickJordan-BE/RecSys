4c049ccb126eb234f496b4d4062b616f05d7b86a
NickJordan-BE <nikjordan525@gmail.com>
2025-11-29 18:21:34 -0800

    Add CORS to model services, create services to call model services from orchestrator service

--- services/orchestrator-service/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
+++ services/orchestrator-service/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
@@ -2,18 +2,9 @@ package com.example.recsys_orchestrator_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
 @SpringBootApplication
 public class RecsysOrchestratorServiceApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "DOne 2";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecsysOrchestratorServiceApplication.class, args);
