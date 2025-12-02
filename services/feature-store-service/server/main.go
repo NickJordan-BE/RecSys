904dc659a1a4c221fa185a6e0fdea03cdff98b73
NickJordan-BE <nikjordan525@gmail.com>
2025-12-02 13:49:35 -0800

    Create feature store service container and add to docker compose

---
+++ services/feature-store-service/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("Hello world from Feature Store")
+	})
+
+	fmt.Println("Server Listening on port 4000")
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
