e57aa7cc434ca6c5bcd2a45ef2c0ebcf28285d56
NickJordan-BE <nikjordan525@gmail.com>
2026-03-24 13:12:04 -0700

    test(ci): test services-build workflow

--- services/orchestrator/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
+++ services/orchestrator/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecsysOrchestratorServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RecsysOrchestratorServiceApplication.class, args);
 	}
 
