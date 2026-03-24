452b0bdff8e80f6c993c41b40a7e9581c2e112e2
NickJordan-BE <nikjordan525@gmail.com>
2026-03-24 13:16:19 -0700

    test(ci): test services-build workflow

--- services/filtering/src/main/java/com/example/filtering_service/FilteringServiceApplication.java
+++ services/filtering/src/main/java/com/example/filtering_service/FilteringServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FilteringServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FilteringServiceApplication.class, args);
 	}
 
