f9ef10b6e7fd36341256f9624b1fdf020d503b95
NickJordan-BE <nikjordan525@gmail.com>
2026-03-24 12:47:13 -0700

    test(ci): test services-build workflow

--- services/filtering/src/main/java/com/example/filtering_service/FilteringServiceApplication.java
+++ services/filtering/src/main/java/com/example/filtering_service/FilteringServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilteringServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FilteringServiceApplication.class, args);
 	}
--- services/orchestrator/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
+++ services/orchestrator/src/main/java/com/example/recsys_orchestrator_service/RecsysOrchestratorServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecsysOrchestratorServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RecsysOrchestratorServiceApplication.class, args);
 	}
