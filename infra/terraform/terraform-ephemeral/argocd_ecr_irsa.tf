293f47b9a9eaf25e47ab07f4cff1c171c6212672
NickJordan-BE <nikjordan525@gmail.com>
2026-03-26 08:18:37 -0700

    refactor(infra): refactor umbrella chart to the app of apps pattern for argocd and remove unnecessary auth for argocd

--- infra/terraform/terraform-ephemeral/argocd_ecr_irsa.tf
+++ infra/terraform/terraform-ephemeral/argocd_ecr_irsa.tf
@@ -1,21 +1,21 @@
 
-module "argocd_ecr_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
-  version = "6.4.0"
-  name    = "Recsys-Argocd-ECR-Role"
+# module "argocd_ecr_irsa_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+#   version = "6.4.0"
+#   name    = "Recsys-Argocd-ECR-Role"
 
-  providers = {
-    aws = aws.eks_admin
-  }
+#   providers = {
+#     aws = aws.eks_admin
+#   }
 
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["argocd:argocd-ecr-sa"]
-    }
-  }
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["argocd:argocd-ecr-sa"]
+#     }
+#   }
 
-  policies = {
-    policy = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  }
-}
+#   policies = {
+#     policy = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   }
+# }
